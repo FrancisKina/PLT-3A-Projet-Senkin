e746e75fa628f580e21d0adc6a3d7b8e28009850
Steven SENG <steven.seng@ensea.fr>
2019-10-15 09:20:35 +0200

    Correction Dia et ajout .h

---
+++ src/shared/state/Character.cpp
@@ -0,0 +1,19 @@
+#include "Character.h"
+
+using namespace state;
+
+int Character::getHpBase(){
+    return hpBase;
+}
+
+int Character::getMovementBase(){
+    return movementBase;
+}
+
+int Character::getInitiativeBase(){
+    return initiativeBase;
+}
+
+int Character::getSkillCountBase(){
+    return skillCountBase;
+}
