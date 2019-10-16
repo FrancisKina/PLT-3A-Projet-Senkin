851bdb9ba644f3e5b901491fd884cd7118a440a5
FrancisEnsea <francis.kadimatanga-kinavuidi@ensea.fr>
2019-10-16 15:02:05 +0200

    skillfactory fini: toutes les attaques sont défini

--- src/shared/state/SkillFactory.cpp
+++ src/shared/state/SkillFactory.cpp
@@ -150,7 +150,7 @@ bool special_COUP_DE_BOULE=0;
 
 Skill SkillFactory::createSkill(SkillId id){
 	Skill move;
-	if (id==SKILL1){
+	if (id==FRAPPE){
 		move.setDamage(damage_FRAPPE);
 		move.setRange(range_FRAPPE);
 		move.setArea(area_FRAPPE);
@@ -158,7 +158,7 @@ Skill SkillFactory::createSkill(SkillId id){
 		move.setEffect(effect_FRAPPE);
 		move.setCooldown(cooldown_FRAPPE);
 		move.setSpecial(special_FRAPPE);
-	}else if (id==SKILL2){
+	}else if (id==SAUT_PIQUE){
 		move.setDamage(damage_SAUTPIQUE);
 		move.setRange(range_SAUTPIQUE);
 		move.setArea(area_SAUTPIQUE);
@@ -167,111 +167,111 @@ Skill SkillFactory::createSkill(SkillId id){
 		move.setCooldown(cooldown_SAUTPIQUE);
 		move.setSpecial(special_SAUTPIQUE);
 	}
-	/*else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}else if (id==){
-		move.setDamage(damage_);
-		move.setRange(range_);
-		move.setArea(area_);
-		move.setPrecision(precision_);
-		move.setEffect(effect_);
-		move.setCooldown(cooldown_);
-		move.setSpecial(special_);
-	}*/
+	else if (id==ARC){
+		move.setDamage(damage_ARC);
+		move.setRange(range_ARC);
+		move.setArea(area_ARC);
+		move.setPrecision(precision_ARC);
+		move.setEffect(effect_ARC);
+		move.setCooldown(cooldown_ARC);
+		move.setSpecial(special_ARC);
+	}else if (id==ARC_EN_CIEL){
+		move.setDamage(damage_ARC_EN_CIEL);
+		move.setRange(range_ARC_EN_CIEL);
+		move.setArea(area_ARC_EN_CIEL);
+		move.setPrecision(precision_ARC_EN_CIEL);
+		move.setEffect(effect_ARC_EN_CIEL);
+		move.setCooldown(cooldown_ARC_EN_CIEL);
+		move.setSpecial(special_ARC_EN_CIEL);
+	}else if (id==SOIN){
+		move.setDamage(damage_SOIN);
+		move.setRange(range_SOIN);
+		move.setArea(area_SOIN);
+		move.setPrecision(precision_SOIN);
+		move.setEffect(effect_SOIN);
+		move.setCooldown(cooldown_SOIN);
+		move.setSpecial(special_SOIN);
+	}else if (id==SOIN_LEGER){
+		move.setDamage(damage_SOIN_LEGER);
+		move.setRange(range_SOIN_LEGER);
+		move.setArea(area_SOIN_LEGER);
+		move.setPrecision(precision_SOIN_LEGER);
+		move.setEffect(effect_SOIN_LEGER);
+		move.setCooldown(cooldown_SOIN_LEGER);
+		move.setSpecial(special_SOIN_LEGER);
+	}else if (id==ENTAILLE){
+		move.setDamage(damage_ENTAILLE);
+		move.setRange(range_ENTAILLE);
+		move.setArea(area_ENTAILLE);
+		move.setPrecision(precision_ENTAILLE);
+		move.setEffect(effect_ENTAILLE);
+		move.setCooldown(cooldown_ENTAILLE);
+		move.setSpecial(special_ENTAILLE);
+	}else if (id==TRANCHARGE){
+		move.setDamage(damage_TRANCHARGE);
+		move.setRange(range_TRANCHARGE);
+		move.setArea(area_TRANCHARGE);
+		move.setPrecision(precision_TRANCHARGE);
+		move.setEffect(effect_TRANCHARGE);
+		move.setCooldown(cooldown_TRANCHARGE);
+		move.setSpecial(special_TRANCHARGE);
+	}else if (id==PIETINEMENT){
+		move.setDamage(damage_PIETINEMENT);
+		move.setRange(range_PIETINEMENT);
+		move.setArea(area_PIETINEMENT);
+		move.setPrecision(precision_PIETINEMENT);
+		move.setEffect(effect_PIETINEMENT);
+		move.setCooldown(cooldown_PIETINEMENT);
+		move.setSpecial(special_PIETINEMENT);
+	}else if (id==FEU_D_ENFER){
+		move.setDamage(damage_FEU_D_ENFER);
+		move.setRange(range_FEU_D_ENFER);
+		move.setArea(area_FEU_D_ENFER);
+		move.setPrecision(precision_FEU_D_ENFER);
+		move.setEffect(effect_FEU_D_ENFER);
+		move.setCooldown(cooldown_FEU_D_ENFER);
+		move.setSpecial(special_FEU_D_ENFER);
+	}else if (id==DOUBLE_LAME){
+		move.setDamage(damage_DOUBLE_LAME);
+		move.setRange(range_DOUBLE_LAME);
+		move.setArea(area_DOUBLE_LAME);
+		move.setPrecision(precision_DOUBLE_LAME);
+		move.setEffect(effect_DOUBLE_LAME);
+		move.setCooldown(cooldown_DOUBLE_LAME);
+		move.setSpecial(special_DOUBLE_LAME);
+	}else if (id==FOUDRE){
+		move.setDamage(damage_FOUDRE);
+		move.setRange(range_FOUDRE);
+		move.setArea(area_FOUDRE);
+		move.setPrecision(precision_FOUDRE);
+		move.setEffect(effect_FOUDRE);
+		move.setCooldown(cooldown_FOUDRE);
+		move.setSpecial(special_FOUDRE);
+	}else if (id==BOULE_DE_FEU){
+		move.setDamage(damage_BOULE_DE_FEU);
+		move.setRange(range_BOULE_DE_FEU);
+		move.setArea(area_BOULE_DE_FEU);
+		move.setPrecision(precision_BOULE_DE_FEU);
+		move.setEffect(effect_BOULE_DE_FEU);
+		move.setCooldown(cooldown_BOULE_DE_FEU);
+		move.setSpecial(special_BOULE_DE_FEU);
+	}else if (id==LANCER_DE_HACHE){
+		move.setDamage(damage_LANCER_DE_HACHE);
+		move.setRange(range_LANCER_DE_HACHE);
+		move.setArea(area_LANCER_DE_HACHE);
+		move.setPrecision(precision_LANCER_DE_HACHE);
+		move.setEffect(effect_LANCER_DE_HACHE);
+		move.setCooldown(cooldown_LANCER_DE_HACHE);
+		move.setSpecial(special_LANCER_DE_HACHE);
+	}else if (id==COUP_DE_BOULE){
+		move.setDamage(damage_COUP_DE_BOULE);
+		move.setRange(range_COUP_DE_BOULE);
+		move.setArea(area_COUP_DE_BOULE);
+		move.setPrecision(precision_COUP_DE_BOULE);
+		move.setEffect(effect_COUP_DE_BOULE);
+		move.setCooldown(cooldown_COUP_DE_BOULE);
+		move.setSpecial(special_COUP_DE_BOULE);
+	}
 	return move;
 }
 	
--- src/shared/state/SkillId.h
+++ src/shared/state/SkillId.h
@@ -6,10 +6,22 @@
 namespace state {
 
   enum SkillId {
-    SKILL1     = 1,
-    SKILL2     = 2,
-    SKILL3     = 4,
-    SKILL4     = 5
+    FRAPPE     = 1,
+    SAUT_PIQUE     = 2,
+    ARC     = 4,
+    ARC_EN_CIEL     = 5,
+    SOIN     = 6,
+    SOIN_LEGER     = 7,
+    ENTAILLE     = 8,
+    TRANCHARGE     = 9,
+    PIETINEMENT     = 10,
+    FEU_D_ENFER     = 11,
+    DOUBLE_LAME     = 12,
+    FOUDRE     = 13,
+    BOULE_DE_FEU     = 14,
+    LANCER_DE_HACHE     = 15,
+    COUP_DE_BOULE     = 3,
+    
   };
 
 };
